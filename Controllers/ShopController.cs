22b92e7169962d39e209c651b4fd71916f0177f3
ReemAtman <reematman15@gmail.com>
2023-04-06 00:08:06 +0200

    First commit for create shop page

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kotabko.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ShopIndex()
+        {
+            return View("ShopIndex");
+        }
+    }
+}
