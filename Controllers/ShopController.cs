4aae37ab48fc13787056d8f6d530998b1cee16fa
ReemAtman <reematman15@gmail.com>
2023-04-10 01:27:49 +0200

    Shop Page V2

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -20,13 +20,40 @@ namespace Kotabko.Controllers
             this.authorRepository = authorRepository;
             this.categoryRepository = categoryRepository;
         }
-        public IActionResult ShopIndex(){
-            
+        public IActionResult ShopIndex(string category,string author ,[FromRouteAttribute] int id)
+        {
             ShopViewModel shopViewModel = new ShopViewModel();
-            shopViewModel.books = mapper.Map<List<MainBookVM>>(bookRepository.GetAll()); 
-            shopViewModel.categoryVMs = mapper.Map<List<CategoryVM>>(categoryRepository.GetAll()); 
+           
+            if (category == null && author == null)
+            {
+                shopViewModel.books = mapper.Map<List<MainBookVM>>(bookRepository.GetAll());
+                
+            }
+            else if(author != null && category == null )
+            {
+                shopViewModel.books = mapper.Map<List<MainBookVM>>(bookRepository.SearchingByAuthor(author));
+                
+            }
+            else if(category != null && author == null)
+            {
+                shopViewModel.books = mapper.Map<List<MainBookVM>>(bookRepository.SearchingByCategories(category));
+                
+            }
+            else
+            {
+                shopViewModel.books = mapper.Map<List<MainBookVM>>(bookRepository.SearchingByAuthorAndCategory(category,author));
+                
+            }
+          
+            shopViewModel.categoryVMs = mapper.Map<List<CategoryVM>>(categoryRepository.GetAll());
             shopViewModel.authorVMs = mapper.Map<List<AuthorVM>>(authorRepository.GetAll());
+            shopViewModel.id = id;
             return View("ShopIndex", shopViewModel);
+        }
+        public IActionResult NextShop(List<MainBookVM> books)
+        {
+           
+            return View(books);
         }
         public IActionResult GetAuthor()
         {
